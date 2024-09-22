da9178174bdefa774e98cbf59a2b2ec859fbcf44
SavindiRashmika <rashmikawadanambi>
2024-09-23 00:16:44 +0530

    maths operater added

---
+++ demo/maths_operater/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+     echo (8+2) .'<br/>';           //10
+     echo (8-2) .'<br/>';           //6
+     echo (8*2) .'<br/>';           //16
+     echo (8/2) .'<br/>';           //4
+     echo (2 ** 3) .'<br/>';        //8
+     echo pow(2,3) .'<br/>';        //8
+     echo sqrt(144) .'<br/>';       //12
+     echo max(5,6,7,8) .'<br/>';    //8
+     echo round(5.3) .'<br/>';      //5
+    ?>
+</body>
+</html>
