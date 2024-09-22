b10e26ac1ce307b5aa319f3124877c2e50be4d5c
SavindiRashmika <rashmikawadanambi>
2024-09-22 23:45:18 +0530

    variable

---
+++ demo/variable/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $name = "kamal";
+        $age = 21;
+        
+        echo "<h1>$name $age</h1>";
+
+    ?>
+</body>
+</html>
