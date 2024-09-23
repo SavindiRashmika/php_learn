759e5849cb94ef8303cc47f408738e7579231d7d
SavindiRashmika <rashmikawadanambi>
2024-09-23 15:29:17 +0530

    condition added

---
+++ demo/conditionals/foreach.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+     $color = array("red", "orange", "black", "blue");
+
+     foreach($color as $value){
+        echo "<h1>$value </h1>"."<br>";
+     }
+    
+    ?>
+</body>
+</html>
--- demo/conditionals/index.php
+++ demo/conditionals/index.php
@@ -9,6 +9,10 @@
     <?php 
         $name ="kamal";
         $read = false;
+        $number = 15;
+
+        // 1 to 5 random numbers
+        $random_num = rand(1,5);
 
         if($read){
             $message = "you have read $name";
@@ -16,10 +20,35 @@
             $message = "you have not read $name";
         }
 
+        switch($number){
+            case 5:
+                echo "Five";
+                break;
+            case 10:
+                echo "Ten";
+                break;
+            default:
+                echo "not"."<br>";
+        }
+
+
+        $num = 1;
+
+        while($num <5){
+            echo "Number is : "."$num"."<br>";
+            $num++;
+        }
+
+
+        $array =include("foreach.php");
+
+        echo "<h1>continue statement</h1>";
+        include("continue.php");
         ?>
 
         <h1>
             <?= "<h1> $message </h1>" ?>
         </h1>
+
 </body>
 </html>
