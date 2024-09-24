cb6931c3190f5e1e76f489faefce075c6560d005
SavindiRashmika <rashmikawadanambi>
2024-09-24 13:47:46 +0530

    website changes

---
+++ test/views/index.view.php
@@ -0,0 +1,16 @@
+<?php require('partials/head.php') ?>
+<div class="min-h-full">
+
+
+<?php require('partials/nav.php') ?>
+
+
+
+  
+  <main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+      <p>hello welcome to the home page</p>
+    </div>
+  </main>
+</div>
+<?php require('partials/footer.php') ?>
