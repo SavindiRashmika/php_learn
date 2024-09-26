e8010b0912691fee754f37b311eba9c06a54b339
SavindiRashmika <rashmikawadanambi>
2024-09-26 09:40:43 +0530

    post data added

--- test/controllers/note-create.php
+++ test/controllers/note-create.php
@@ -1,10 +1,18 @@
 <?php
 
+$config = require('config.php');
+$db = new Database($config['database']);
 
 $heading = 'Create Note';
 
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Next episode: Create new note using form data from $_POST.
+if (!empty($_POST['title'])) {
+    $db->query('INSERT INTO posts (title, user_id) VALUES (:title, :user_id)', [
+        'title' => $_POST['title'],
+        'user_id' => 1
+    ]);
+} else {
+    echo "Title cannot be empty!";
 }
+
 require 'views/note-create.view.php';
